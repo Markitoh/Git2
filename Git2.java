da8e0a94e9f7e4c3c7fe79578dd2ca7c4fbe5a5c
Markitoh <51713413+Markitoh@users.noreply.github.com>
2019-06-12 13:08:38 -0400

    Add files via upload

--- Git2.java
+++ Git2.java
@@ -2,7 +2,7 @@ public class Git2 {
 
 	public static void main (String args[]) {
 		
-	System.out.println("Mi nombre es /n" + args[0] + args[1] + " " + "Y tengo /n" + args[2]+ ", trabajo en" + " "+ args[3]);
+	System.out.println("Mi nombre es"+" " + args[0] +" "+ args[1] + " " + "Y tengo" +" " + args[2]+" "+ "de edad" + ", trabajo en" + " "+ args[3]);
 	}
 	
 }
