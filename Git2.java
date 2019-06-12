a8e232b609afddfd1d4ce19aa25ba0325b74ab6e
Markitoh <51713413+Markitoh@users.noreply.github.com>
2019-06-12 13:01:57 -0400

    Add files via upload

--- Git2.java
+++ Git2.java
@@ -1,6 +1,6 @@
 public class Git2 {
 
-	public static void main (String [] args) {
+	public static void main (String args[]) {
 		
 	System.out.println(args[0]+" ");
 	System.out.println(args[1]+" ");
